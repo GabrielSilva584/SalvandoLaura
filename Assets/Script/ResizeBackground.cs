8245e49c2ca0c0650d6bfbcd88eff83b0f45e401
Gabriel Martins da Silva <gms.1997@hotmail.com>
2017-11-01 08:26:09 -0200

    Correções leves

---
+++ Assets/Script/ResizeBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var rt = GetComponent<RectTransform>();
+		if (rt == null) return;
+
+		//rt.sizeDelta =  new Vector2 (Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
